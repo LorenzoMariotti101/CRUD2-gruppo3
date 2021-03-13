e7d0befa80010af9db26c22728df271976a51df3
LorenzoMariotti <lorenzomariotti101@gmail.com>
2021-03-13 12:08:30 +0100

    Risoluzione ticket #1

---
+++ CRUD2-gruppo3-server/src/main/java/it/iad/CRUD2gruppo3/Crud2Gruppo3Application.java
@@ -0,0 +1,13 @@
+package it.iad.CRUD2gruppo3;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Crud2Gruppo3Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Crud2Gruppo3Application.class, args);
+	}
+
+}
---
+++ CRUD2-gruppo3-server/src/test/java/it/iad/CRUD2gruppo3/Crud2Gruppo3ApplicationTests.java
@@ -0,0 +1,13 @@
+package it.iad.CRUD2gruppo3;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Crud2Gruppo3ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
